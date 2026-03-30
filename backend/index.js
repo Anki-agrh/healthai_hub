2dc31ac431015ed1385755cd46d8579c39213dd4
Anki-agrh <ankitaagrahari005@gmail.com>
2026-03-31 02:18:51 +0530

    Fix: of nodemailer

--- backend/index.js
+++ backend/index.js
@@ -46,13 +46,12 @@ const allowedOrigins = [
 // EMAIL CONFIGURATION
 // =====================
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", 
-  port: 465,              
-  secure: true,           
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER, 
     pass: process.env.EMAIL_PASS, 
   },
+});
 
 transporter.verify(function (error, success) {
   if (error) {
